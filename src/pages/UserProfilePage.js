48cb090cfbc42b5befa7382c18a101c0f0d86325
Fernando Ramos <fndo.ramos@gmail.com>
2023-06-30 20:35:03 +0200

    add user profile basic functionality

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,48 +1,62 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import ActivityCard from "../components/ActivityCard";
 
 export default function UserProfilePage() {
-	const { username } = useParams();
-	const [activities, setActivities] = useState(null);
-	const API_URL = process.env.REACT_APP_API_URL;
+  const { username } = useParams();
+  const [activities, setActivities] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
+  const API_URL = process.env.REACT_APP_API_URL;
 
-	const getActivitiesOfUser = () => {
-		let activitiesArr = [];
+  const getActivitiesOfUser = () => {
+    let activitiesArr;
 
-		axios
-			.get(`${API_URL}/api/activities`)
-			.then((response) => {
-				console.log(response, "thiss is the activities array");
-				activitiesArr.push(response.data);
-				return axios.get(`${API_URL}/users/${username}`);
-			})
-			.then((userDetails) => {
-				console.log(userDetails, "this is the the user data");
-				setActivities(
-					activitiesArr.filter((activity) => {
-						return userDetails._id === activity.user._id;
-					})
-				);
-				console.log(activities, "this is the end");
-			})
-			.catch((error) =>
-				console.log("error getting the list of activities", error)
-			);
-	};
+    axios
+      .get(`${API_URL}/api/activities`)
+      .then((response) => {
+        activitiesArr = response.data;
+        return axios.get(`${API_URL}/api/users/${username}`);
+      })
+      .then((userDetails) => {
+        setCurrentUser(userDetails.data[0]);
+        const filteredArr = activitiesArr.filter((activity) => {
+          return userDetails.data[0]._id === activity.user._id;
+        });
+        setActivities(filteredArr);
+      })
+      .catch((error) =>
+        console.log("error getting the list of activities", error)
+      );
+  };
 
-	useEffect(() => {
-		getActivitiesOfUser();
-	}, []);
+  useEffect(() => {
+    getActivitiesOfUser();
+  }, []);
 
-	return (
-		<div>
-			<div>
-				{/* <img src={imgProfile} /> */}
-				<p>username</p>
-				<p>first name</p>
-				<p>last name</p>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div>
+        {!currentUser ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <img src={currentUser.imgProfile} />
+            <p>{currentUser.username}</p>
+            <p>{currentUser.firstName}</p>
+            <p>{currentUser.lastName}</p>
+          </div>
+        )}
+      </div>
+      <div>
+        {!activities ? (
+          <p>Loading...</p>
+        ) : (
+          activities.map((activity) => {
+            return <ActivityCard key={activity._id} {...activity} />;
+          })
+        )}
+      </div>
+    </div>
+  );
 }
