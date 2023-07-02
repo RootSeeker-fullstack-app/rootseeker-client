16cd5eb2acf31b6e74ef519385a83d43e847613c
Fernando Ramos <fndo.ramos@gmail.com>
2023-07-02 17:38:41 +0200

    fixed when logout redirect to homepage

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const API_URL = process.env.REACT_APP_API_URL;
 
 const AuthContext = React.createContext();
 
 function AuthProviderWrapper(props) {
+
+	const navigate = useNavigate();
+
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
 	const [user, setUser] = useState(null);
@@ -54,6 +59,7 @@ function AuthProviderWrapper(props) {
 	const logOutUser = () => {
 		removeToken();
 		authenticateUser();
+		navigate("/")
 	};
 
 	useEffect(() => {
