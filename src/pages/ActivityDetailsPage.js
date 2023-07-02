24561f4896fb4cf599d791458afe83aac95e36e0
Fernando Ramos <fndo.ramos@gmail.com>
2023-07-02 21:50:43 +0200

    modify activity details page to render reservations or edit and delete buttons

--- src/pages/ActivityDetailsPage.js
+++ src/pages/ActivityDetailsPage.js
@@ -1,75 +1,97 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import MakeReservationForm from "../components/MakeReservationForm";
+import { AuthContext } from "../context/auth.context";
+import { Button } from "react-daisyui";
 
 function ActivityDetailsPage() {
-	const [activity, setActivity] = useState(null);
-	const [showReservationForm, setShowReservationForm] = useState(false);
+  const [activity, setActivity] = useState(null);
+  const [showReservationForm, setShowReservationForm] = useState(false);
 
-	const { activityId } = useParams();
+  const { activityId } = useParams();
 
-	const getActivity = () => {
-		axios
-			.get(`${process.env.REACT_APP_API_URL}/api/activities/${activityId}`)
-			.then((response) => {
-				setActivity(response.data);
-			})
-			.catch((error) =>
-				console.log("Error getting details from de API", error)
-			);
-	};
+  const { user } = useContext(AuthContext);
 
-	useEffect(() => {
-		getActivity();
-	}, []);
+  const getActivity = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/api/activities/${activityId}`)
+      .then((response) => {
+        setActivity(response.data);
+      })
+      .catch((error) =>
+        console.log("Error getting details from de API", error)
+      );
+  };
 
-	const reservationFormState = () => {
-		setShowReservationForm(!showReservationForm);
-	};
+  useEffect(() => {
+    getActivity();
+  }, []);
 
-	return (
-		<div>
-			{!activity ? (
-				<p>Loading...</p>
-			) : (
-				<div className="flex flex-row">
-					<div className="basis-1/5"></div>
-					<div className="grid-cols-2 basis-3/5">
-						<div className="grid grid-cols-5 gap-4">
-							<div className="col-span-2 bg-gray-200">
-								<h1>{activity.name}</h1>
-							</div>
-							<div className="hidden col-span-3 bg-gray-300">Column empty</div>
-							<div className="col-span-5 bg-gray-400">
-								<p>{activity.description}</p>
-							</div>
-							<div className="col-span-2 bg-gray-400">
-								<h3>Duration: {activity.duration}min</h3>
+  const reservationFormState = () => {
+    setShowReservationForm(!showReservationForm);
+  };
 
-								<h3>Available: {activity.available}</h3>
-								<h3>Date: {activity.date.slice(0, 10)}</h3>
-								<h3>Price: {activity.price}€</h3>
-								{showReservationForm && <MakeReservationForm {...activity} />}
+  return (
+    <div>
+      {!activity ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="flex flex-row">
+          <div className="basis-1/5"></div>
+          <div className="grid-cols-2 basis-3/5">
+            <div className="grid grid-cols-5 gap-4">
+              <div className="col-span-2 bg-gray-200">
+                <h1>{activity.name}</h1>
+              </div>
+              <div className="hidden col-span-3 bg-gray-300">Column empty</div>
+              <div className="col-span-5 bg-gray-400">
+                <p>{activity.description}</p>
+              </div>
+              <div className="col-span-2 bg-gray-400">
+                <h3>Duration: {activity.duration}min</h3>
 
-								{showReservationForm ? (
-									<button onClick={reservationFormState}>Hide Form</button>
-								) : (
-									<button onClick={reservationFormState}>
-										Make reservation
-									</button>
-								)}
-							</div>
-							<div className="col-span-3 bg-gray-400">
-								<img src={activity.images} alt="imageName" />
-							</div>
-						</div>
-					</div>
-					<div className="basis-1/5"></div>
-				</div>
-			)}
-		</div>
-	);
+                <h3>Available: {activity.available}</h3>
+                <h3>Date: {activity.date.slice(0, 10)}</h3>
+                <h3>Price: {activity.price}€</h3>
+                <Link to={`/profile/${activity.user.username}`}>
+                  <h3>Host: {activity.user.username} </h3>
+                </Link>
+                {user.username !== activity.user.username ? (
+                  <>
+                    {showReservationForm && (
+                      <MakeReservationForm {...activity} />
+                    )}
+
+                    {showReservationForm ? (
+                      <Button className="btn btn-primary" onClick={reservationFormState}>Hide Form</Button>
+                    ) : (
+                      <Button className="btn btn-primary" onClick={reservationFormState}>
+                        Make reservation
+                      </Button>
+                    )}
+                  </>
+                ) : (
+                  <>
+                    <Button className="btn btn-primary">
+                      Edit
+                    </Button>
+                    <Button className="btn btn-error">
+                      Delete
+                    </Button>
+                  </>
+                )}
+              </div>
+              <div className="col-span-3 bg-gray-400">
+                <img src={activity.images} alt="imageName" />
+              </div>
+            </div>
+          </div>
+          <div className="basis-1/5"></div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default ActivityDetailsPage;
