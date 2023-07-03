5ef7046f45c4af0b30d48df242164bf6f10fd57b
gtildis <gretil_15@hotmail.com>
2023-07-03 19:09:41 +0200

    add homepage stylling

---
+++ src/components/ChooseCard.js
@@ -0,0 +1,50 @@
+import { useSpring, animated } from "@react-spring/web";
+// import { Link } from "react-router-dom";
+import { Button } from "react-daisyui";
+
+export default function HomeCard() {
+	const springs = useSpring({
+		from: { x: "-800%" },
+		to: { x: "120%" },
+	});
+
+	return (
+		<animated.div
+			style={{
+				width: 20 + "rem",
+				height: 20 + "rem",
+				// background: "#ff6d6d",
+				borderRadius: 8,
+				...springs,
+			}}
+		>
+			<div className="card w-96 bg-neutral text-neutral-content">
+				<div className="relative items-center text-center card-body">
+					<h2 className="card-title">
+						With RootSeeker you can track activities all over the World
+					</h2>
+					<p>
+						With Rootseeker you can meet new people and find cool outdoor
+						activities in your city or in the nature nearby
+					</p>
+					<div className="justify-end card-actions">
+						<h1>Choose your category</h1>
+
+						<Button color="ghost" className="btn-xs">
+							Activities
+						</Button>
+						<Button color="ghost" className="btn-xs">
+							Activities
+						</Button>
+						<Button color="ghost" className="btn-xs">
+							Activities
+						</Button>
+						<Button color="ghost" className="btn-xs">
+							Activities
+						</Button>
+					</div>
+				</div>
+			</div>
+		</animated.div>
+	);
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,68 @@
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
+import SearchBarComp from "../components/SearchBarComp";
+import WelcomeCard from "../components/WelcomeCard";
+import StickyCard from "../components/StickyCard";
+import ChooseCard from "../components/ChooseCard";
+import FooterCard from "../components/FooterCard";
+
 export default function HomePage() {
-	return <div>This is the homepage!</div>;
+	const alignCenter = { display: "flex", alignItems: "center" };
+	return (
+		<div className="flex justify-center">
+			<div className="justify-center w-1/2 ">
+				<SearchBarComp />
+			</div>
+			<div />
+
+			<Parallax pages={5}>
+				<ParallaxLayer
+					offset={0}
+					speed={0.5}
+					style={{ ...alignCenter, justifyContent: "center" }}
+				>
+					<WelcomeCard />
+				</ParallaxLayer>
+
+				<ParallaxLayer
+					className=""
+					sticky={{ start: 1, end: 3 }}
+					style={{ ...alignCenter, justifyContent: "flex-start" }}
+				>
+					<div>
+						<StickyCard />
+					</div>
+				</ParallaxLayer>
+
+				<ParallaxLayer
+					className="right-0"
+					offset={1.5}
+					speed={1.5}
+					style={{ ...alignCenter, justifyContent: "center" }}
+				>
+					<div>
+						<ChooseCard />
+					</div>
+				</ParallaxLayer>
+
+				<ParallaxLayer
+					offset={2.5}
+					speed={1.5}
+					style={{ ...alignCenter, justifyContent: "center" }}
+				>
+					<div>
+						<ChooseCard />
+					</div>
+				</ParallaxLayer>
+				<ParallaxLayer
+					sticky={{ start: 3.5, end: 5 }}
+					style={{ ...alignCenter, justifyContent: "center" }}
+				>
+					<div className="w-11/12 h-72">
+						<FooterCard />
+					</div>
+				</ParallaxLayer>
+			</Parallax>
+		</div>
+	);
 }
