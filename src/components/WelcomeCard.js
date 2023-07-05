1bc2809588bfcaaaf737e1eb681e4b3233449029
gtildis <gretil_15@hotmail.com>
2023-07-05 12:56:53 +0200

    fix content on homepage cards

--- src/components/WelcomeCard.js
+++ src/components/WelcomeCard.js
@@ -1,19 +1,19 @@
 export default function WelcomeCard() {
 	return (
-		<div className="min-h-screen hero bg-base-200">
+		<div
+			className="h-screen text-black hero"
+			style={{ backgroundColor: "#22A699" }}
+		>
 			<div className="flex-col hero-content lg:flex-row">
 				<img
 					src="https://www.windsorgreatpark.co.uk/wp-content/uploads/2022/11/38.-Swinley-Forest-scaled.jpg"
-					className="max-w-sm rounded-lg shadow-2xl"
+					className="max-w-xl rounded-lg shadow-2xl"
 				/>
 				<div>
-					<h1 className="text-5xl font-bold">Welcome To RootSeeker</h1>
-					<p className="py-6">
-						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-						excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-						a id nisi.
+					<h1 className="p-4 text-8xl">Welcome To RootSeeker</h1>
+					<p className="py-6 text-xl text-right">
+						...A window to outdoor oportunities
 					</p>
-					<button className="btn btn-primary">Get Started</button>
 				</div>
 			</div>
 		</div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,20 +3,23 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import SearchBarComp from "../components/SearchBarComp";
 import WelcomeCard from "../components/WelcomeCard";
 import StickyCard from "../components/StickyCard";
-import ChooseCard from "../components/ChooseCard";
+import ChooseCard from "../components/HostCard";
 import FooterCard from "../components/FooterCard";
+import HostCard from "../components/ChooseCard";
 
 export default function HomePage() {
 	const alignCenter = { display: "flex", alignItems: "center" };
+	const url =
+		"https://images.pexels.com/photos/17201962/pexels-photo-17201962/free-photo-of-landscape-sunset-field-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 	return (
 		<div className="flex justify-center">
 			<div className="z-40 justify-center w-1/2 ">
 				<SearchBarComp />
 			</div>
-			<div />
 
 			<Parallax pages={5}>
 				<ParallaxLayer
+					className="z-30"
 					offset={0}
 					style={{
 						...alignCenter,
@@ -26,43 +29,61 @@ export default function HomePage() {
 				>
 					<WelcomeCard />
 				</ParallaxLayer>
-
 				<ParallaxLayer
-					className=""
+					// className="bg-green-200"
 					sticky={{ start: 1, end: 3 }}
-					style={{ ...alignCenter, justifyContent: "flex-start" }}
+					style={{
+						...alignCenter,
+						justifyContent: "flex-start",
+					}}
 				>
-					<div>
-						<StickyCard />
-					</div>
+					<StickyCard />
+				</ParallaxLayer>
+				<ParallaxLayer
+					offset={0.5}
+					factor={4.5}
+					style={{
+						// backgroundImage: `url(https://images.pexels.com/photos/17201962/pexels-photo-17201962/free-photo-of-landscape-sunset-field-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`,
+						backgroundSize: "contain",
+						backgroundColor: "#F2BE22",
+					}}
+				>
+					<ParallaxLayer
+						className="right-0"
+						offset={1.5}
+						speed={1.5}
+						style={{
+							...alignCenter,
+							justifyContent: "center",
+							zIndex: 2,
+						}}
+					>
+						<ChooseCard />
+					</ParallaxLayer>
+
+					<ParallaxLayer
+						offset={2.5}
+						speed={1.5}
+						style={{
+							...alignCenter,
+							justifyContent: "center",
+							zIndex: 30,
+						}}
+					>
+						<HostCard />
+					</ParallaxLayer>
 				</ParallaxLayer>
 
-				<ParallaxLayer
-					className="right-0"
-					offset={1.5}
-					speed={1.5}
-					style={{ ...alignCenter, justifyContent: "center" }}
-				>
-					<div>
-						<ChooseCard />
-					</div>
-				</ParallaxLayer>
-
-				<ParallaxLayer
-					offset={2.5}
-					speed={1.5}
-					style={{ ...alignCenter, justifyContent: "center" }}
-				>
-					<div>
-						<ChooseCard />
-					</div>
-				</ParallaxLayer>
 				<ParallaxLayer
 					offset={4}
-					style={{ ...alignCenter, justifyContent: "center" }}
+					style={{
+						...alignCenter,
+						justifyContent: "center",
+						backgroundColor: "#F29727",
+					}}
 				>
-					<div class="min-h-screen flex flex-col">
-						<div class="flex-grow"></div>
+					<div className="flex flex-col min-h-screen">
+						<div className="flex-grow"></div>
 
 						<FooterCard className="flex flex-col max-w-screen flex-end" />
 					</div>
